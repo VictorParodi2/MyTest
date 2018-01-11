aa8a496bcbcceb03e61d87e49adb308632008ad2
Victor Parodi <victorparodi2@gmail.com>
2018-01-11 16:32:17 -0500

    Line from onwer added

--- app.js
+++ app.js
@@ -2,3 +2,5 @@ console.log('LINE 1: FROM OWNER');
 console.log('LINE 2: FROM OWNER');
 console.log('Line 3: from contributor');
 console.log('Line 4: from contributor');
+console.log('LINE: FROM OWNER');
+
