b8817d9bdc50734c5a96388a5d12affe57e6949b
Victor Parodi <victorparodi2@gmail.com>
2018-01-11 16:51:01 -0500

    Line 6 added

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ console.log('Line 3: from contributor');
 console.log('Line 4: from contributor');
 console.log('LINE: FROM OWNER');
 console.log('Line 5: from contributor');
+console.log('Line 6: from contributor');
+
 
 
 
